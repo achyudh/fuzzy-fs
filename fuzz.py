2b65daaac15ae4e34611b32c4f5df3fe0332b3b3
Achyudh Ram <achyudhk@gmail.com>
2017-04-18 23:32:01 +0530

    Add 20NG dataset

--- fuzz.py
+++ fuzz.py
@@ -6,25 +6,20 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
-from skcmeans.algorithms import Probabilistic
 from sklearn.datasets import make_blobs
 
-data = pd.read_table("20NG/training.txt") 
-data.columns=["Y","X"]
+data = pd.read_table("data/20NG/training.txt") 
+data.columns = ["Y", "X"]
 Y = data["Y"]
 X = data["X"]
 
-x_train, X, y_train, Y = train_test_split(
-    X, Y, test_size=0.1)
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
 
 vectorizer = TfidfVectorizer()
-X = vectorizer.fit_transform(X)
-
-x_train, x_test, y_train, y_test = train_test_split(
-    X, Y, test_size=0.2)
+x_train = vectorizer.fit_transform(x_train)
     
 print(x_train.shape)
 print(y_train.shape)
 
-cntr, u, u0, d, jm, p, fpc = fuzz.cluster.cmeans(
-        x_train, 20, 2, error=0.005, maxiter=1000, init=None)
+# cntr, u, u0, d, jm, p, fpc = fuzz.cluster.cmeans(
+#         x_train, 20, 2, error=0.005, maxiter=1000, init=None)
