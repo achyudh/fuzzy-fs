de097ec8e02a013cd38be4a1d36880dad8aee5be
Achyudh Ram <achyudhk@gmail.com>
2017-04-25 06:22:31 +0530

    Update Script

--- fuzzy_feature_selection.py
+++ fuzzy_feature_selection.py
@@ -1,38 +1,48 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import skfuzzy as fuzz
 import pandas as pd
-from util.custom_stopwords import custom_stopword_set
-from nltk.corpus import stopwords
-from nltk.stem.snowball import SnowballStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
-from sklearn.model_selection import train_test_split
-from sklearn.datasets import make_blobs
+from tqdm import tqdm
+from sklearn.cluster import KMeans
 
-stopword_set = set(stopwords.words('english')) | custom_stopword_set
-stemmer = SnowballStemmer("english")
+def random_class_selection(data):
+    classes = ['project', 'faculty', 'course', 'student']
+    #np.random.shuffle(classes)
+    group5 = data.loc[data['Y'] == classes[0]]
+    group5 = group5.append(data.loc[data['Y'] == classes[1]], ignore_index=True)
+    group5 = group5.append(data.loc[data['Y'] == classes[2]], ignore_index=True)
+    return group5.append(data.loc[data['Y'] == classes[3]], ignore_index=True)
 
-data = pd.read_table("data/20NG/training.txt") 
-data.columns = ["Y", "X"]
+
+data = pd.read_table("data/WebKB/webkb-train-stemmed.txt")
+data.columns=["Y","X"]
+data = random_class_selection(data)
 Y = data["Y"]
 X = data["X"]
-X_post = list()
-
-# Stopword Removal and Stemming
-for xi in X:
-    X_post.append([stemmer.stem(xij) for xij in xi.split() if xij not in stopword_set])
-for index in range(len(X_post)):
-    X_post[index] = " ".join([xij for xij in X_post[index] if xij not in stopword_set])
-
-x_train, x_test, y_train, y_test = train_test_split(X_post, Y, test_size=0.2)
-
 vectorizer = TfidfVectorizer()
-x_train = vectorizer.fit_transform(x_train)
+# counter = CountVectorizer()
+# Xc = counter.fit_transform(X).toarray()
+X = vectorizer.fit_transform(X.values.astype('U')).toarray()
 
-print(x_train.shape)
-print(y_train.shape)
+x_train = X
+y_train = Y
 
-# cntr, u, u0, d, jm, p, fpc = fuzz.cluster.cmeans(
-#         x_train, 20, 2, error=0.005, maxiter=1000, init=None)
+# kmeans = KMeans(n_clusters=4,n_jobs=-1).fit(x_train)
+# print('Score:',metrics.adjusted_rand_score(y_train,kmeans.predict(x_train)))
+
+cntr, u, u0, d, jm, p, fpc = fuzz.cluster.cmeans(x_train.T, 16, 4, error=0.0001, maxiter=100000, init=None)
+print('Score:',metrics.adjusted_rand_score(y_train,np.argmax(u,axis=0)))
+
+docinc = [[] for x in range(5)]
+
+for k in range(x_train.shape[0]):
+    docinc[np.argmax(u[:,k])].append(k)
+clu = 0
+CF = np.zeros((x_train.shape[1],x_train.shape[1]))
+for i in tqdm(range(x_train.shape[1])):
+    for j in range(x_train.shape[1]):
+        summ=0
+        for k in docinc[clu]:
+            summ+=X[k][i]*X[k][j]
+        CF[i,j]=summ
